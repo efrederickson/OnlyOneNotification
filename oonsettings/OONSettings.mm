34da0a7c9dc26d0d34484becf4bbb86efb416d7c
Elijah Frederickson <efrederickson@Elijahs-Mac-mini.local>
2014-01-07 20:07:59 -0900

    Added developer information to settings page ;)

--- oonsettings/OONSettings.mm
+++ oonsettings/OONSettings.mm
@@ -11,6 +11,33 @@
 	}
 	return _specifiers;
 }
+
+-(void) openTwitter
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"twitter://user?screen_name=mlnlover11"]];
+}
+
+-(void) openGithub
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"github.com/mlnlover11"]];
+}
+
+-(void) sendEmail
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:elijah.frederickson@gmail.com?subject=OnlyOneNotification"]];
+}
+
+-(void) donatePaypal
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=6ZLWPUSTR7XLQ&lc=US&item_name=OnlyOneNotification%20Donations&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"]];
+    
+}
+
+-(void) donateBitcoin
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://coinbase.com/checkouts/9066da6836ac005ad852734fa3567288"]];
+}
+
 @end
 
 // vim:ft=objc
